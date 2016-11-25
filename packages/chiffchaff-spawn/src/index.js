ef9f41484390d07097a85e7b30d54a5a66103fc7
Tim De Pauw <timdp@users.noreply.github.com>
2016-11-25 13:36:00 +0100

    Replace deprecated cross-spawn-async with cross-spawn

--- packages/chiffchaff-spawn/src/index.js
+++ packages/chiffchaff-spawn/src/index.js
@@ -6,7 +6,7 @@ const debug = _debug('chiffchaff:SpawnTask')
 import Task from 'chiffchaff'
 import EventRegistry from 'event-registry'
 import defaults from 'defaults'
-import spawn from 'cross-spawn-async'
+import spawn from 'cross-spawn'
 import Promise from 'bluebird'
 import {Stream} from 'stream'
 
